89b46233fadee386c4a0856126548a73c6441469
Cherkani <cherkaniaymen1@gmail.com>
2023-06-02 16:54:24 +0100

    version phone

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-
 import "./styles.css";
 import ParentComponent from "../../components/Home/FilterPanel/ParentComponent";
 
